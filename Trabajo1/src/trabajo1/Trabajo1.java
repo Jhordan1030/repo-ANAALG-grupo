f905f03234787cafa61404d3443e63fc391583ab
Jhordan1030 <jhordanhuera2002@gmail.com>
2024-10-29 12:08:22 -0500

    actualizacion en main

--- Trabajo1/src/trabajo1/Trabajo1.java
+++ Trabajo1/src/trabajo1/Trabajo1.java
@@ -1,9 +1,10 @@
-package trabajo1;
+ package trabajo1;
 
 public class Trabajo1 {
 
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("Menu");
     }
     
 }
