a6884586eadb1068e494c6b346af5e3f704abe0d
giuliana1105 <gaespinozab@utn.edu.ec>
2024-10-30 10:03:48 -0700

    ejercicios 4,5,6 subidos sin main

---
+++ Trabajo1/src/trabajo1/Ejercicio5.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajo1;
+
+/**
+ *
+ * @author Giuliana Espinoza
+ */
+public class Ejercicio5 {
+  
+
+/**
+ * Clase que representa una matriz cuadrada y permite llenarla en un patrón diagonal.
+ * Inicia en la diagonal central y continúa hacia las diagonales superiores e inferiores.
+ * 
+ * @author Giuliana Espinoza
+ */
+
+    private int[][] matriz;
+    private int tamano;
+
+    /**
+     * Constructor para inicializar la matriz con el tamaño especificado.
+     * @param tamano El tamaño de la matriz cuadrada.
+     */
+    public Ejercicio5(int tamano) {
+        this.tamano = tamano;
+        this.matriz = new int[tamano][tamano];
+    }
+
+    /**
+     * Genera un patrón en diagonal comenzando desde la diagonal central.
+     * Llena primero la diagonal central, luego las diagonales inferiores y superiores.
+     */
+    public void generarDiagonales() {
+        int contador = 1;
+
+        llenarDiagonalCentral(contador);
+        contador += tamano;  // Ajustar contador para diagonales inferiores
+
+        llenarDiagonalesInferiores(contador);
+        contador += tamano * (tamano - 1) / 2;  // Ajustar para diagonales superiores
+
+        llenarDiagonalesSuperiores(contador);
+    }
+
+    private void llenarDiagonalCentral(int contador) {
+        for (int fila = tamano - 1, columna = 0; fila >= 0 && columna < tamano; fila--, columna++) {
+            matriz[fila][columna] = contador++;
+        }
+    }
+
+    private void llenarDiagonalesInferiores(int contador) {
+        for (int inicio = 1; inicio < tamano; inicio++) {
+            int fila = tamano - 1;
+            int columna = inicio;
+            while (fila >= inicio && columna < tamano) {
+                matriz[fila][columna] = contador++;
+                fila--;
+                columna++;
+            }
+        }
+    }
+
+    private void llenarDiagonalesSuperiores(int contador) {
+        for (int inicio = 1; inicio < tamano; inicio++) {
+            int fila = tamano - 1 - inicio;
+            int columna = 0;
+            while (fila >= 0 && columna < tamano - inicio) {
+                matriz[fila][columna] = contador++;
+                fila--;
+                columna++;
+            }
+        }
+    }
+
+    /**
+     * Muestra la matriz en formato tabular.
+     */
+    public void mostrarMatriz() {
+        for (int i = 0; i < tamano; i++) {
+            for (int j = 0; j < tamano; j++) {
+                System.out.printf("%4d", matriz[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
+
+    
+
