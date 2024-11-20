9eb8caa9ea3861099cd81835bced88d50cd28e48
jhordan1030 <jhordanhuera2002@gmail.com>
2024-11-19 21:21:47 -0500

    actualizacion de ejercicio1

--- Examen1/src/com/Inicial.java
+++ Examen1/src/com/Inicial.java
@@ -3,18 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com;
+import java.util.Arrays;
+import  java.util.Scanner;
 
-/**
- *
- * @author jhord
- */
 public class Inicial {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
